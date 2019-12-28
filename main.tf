833ff513dedc6f0220b0adce74d87c97a1b97ca9
Felipe Frizzo <felipefrizzo@gmail.com>
2019-12-28 11:26:38 -0300

    Convert to terraform 0.12

--- main.tf
+++ main.tf
@@ -1,29 +1,29 @@
 module "s3-bucket" {
   source        = "git::https://github.com/felipefrizzo/terraform-aws-s3-bucket.git?ref=master"
-  bucket_name   = "${var.bucket_name}"
-  force_destroy = "${var.force_destroy}"
-  public        = "${var.public_bucket}"
-  versioned     = "${var.versioned}"
+  bucket_name   = var.bucket_name
+  force_destroy = var.force_destroy
+  public        = var.public_bucket
+  versioned     = var.versioned
 }
 
 resource "aws_transfer_server" "transfer_server" {
   identity_provider_type = "SERVICE_MANAGED"
-  logging_role           = "${aws_iam_role.transfer_server_role.arn}"
+  logging_role           = aws_iam_role.transfer_server_role.arn
 
   tags {
-    NAME = "${var.transfer_server_name}"
+    NAME = var.transfer_server_name
   }
 }
 
 resource "aws_transfer_user" "transfer_server_user" {
-  server_id      = "${aws_transfer_server.transfer_server.id}"
-  user_name      = "${var.transfer_server_user_name}"
-  role           = "${aws_iam_role.transfer_server_role.arn}"
+  server_id      = aws_transfer_server.transfer_server.id
+  user_name      = var.transfer_server_user_name
+  role           = aws_iam_role.transfer_server_role.arn
   home_directory = "/${module.s3-bucket.s3_bucket_id}"
 }
 
 resource "aws_transfer_ssh_key" "transfer_server_ssh_key" {
-  server_id = "${aws_transfer_server.transfer_server.id}"
-  user_name = "${aws_transfer_user.transfer_server_user.user_name}"
-  body      = "${var.transfer_server_ssh_key}"
+  server_id = aws_transfer_server.transfer_server.id
+  user_name = aws_transfer_user.transfer_server_user.user_name
+  body      = var.transfer_server_ssh_key
 }
--- variables.tf
+++ variables.tf
@@ -1,33 +1,37 @@
 variable "bucket_name" {
   description = "The S3 bucket name"
+  type        = string
 }
 
 variable "force_destroy" {
   description = "Delete all objects in bucket on destroy"
-  type = "boolean"
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "public_bucket" {
   description = "Allow public read access to bucket"
-  type = "boolean"
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "versioned" {
   description = "Version the bucket"
-  type = "boolean"
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "transfer_server_name" {
   description = "Transfer Server name"
+  type        = string
 }
 
 variable "transfer_server_user_name" {
   description = "User name for SFTP server"
+  type        = string
 }
 
 variable "transfer_server_ssh_key" {
   description = "SSH Key for transfer server user"
+  type        = string
 }
