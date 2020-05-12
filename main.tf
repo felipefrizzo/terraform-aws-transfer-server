0acd765cd0ba67973edefb1387f68cd201fe4247
Felipe Frizzo <felipefrizzo@gmail.com>
2020-05-12 08:56:08 -0300

    Fixes transfer server tags block

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_transfer_server" "transfer_server" {
   identity_provider_type = "SERVICE_MANAGED"
   logging_role           = aws_iam_role.transfer_server_role.arn
 
-  tags {
+  tags = {
     NAME = var.transfer_server_name
   }
 }
